0c7551ddf6061f6ac24978d74e4e42c7b8da0339
Adrien <amuller@agilepartner.net>
2020-01-24 12:12:06 +0100

    lombok

--- src/main/java/java4/tothefuture/from7to8/Person.java
+++ src/main/java/java4/tothefuture/from7to8/Person.java
@@ -1,5 +1,8 @@
 package java4.tothefuture.from7to8;
 
+import lombok.Data;
+
+@Data
 public class Person {
     private Integer salary;
     private String name;
@@ -8,30 +11,6 @@ public class Person {
     public Person(Integer salary, String name, Sex sex) {
         this.salary = salary;
         this.name = name;
-        this.sex = sex;
-    }
-
-    public Integer getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Integer age) {
-        this.salary = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Sex getSex() {
-        return sex;
-    }
-
-    public void setSex(Sex sex) {
         this.sex = sex;
     }
 }
