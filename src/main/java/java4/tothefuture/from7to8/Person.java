04cd4763f749561243c80cad9d8189fb6fae1201
Adrien <amuller@agilepartner.net>
2020-01-24 11:23:27 +0100

    test stream

---
+++ src/main/java/java4/tothefuture/from7to8/Person.java
@@ -0,0 +1,27 @@
+package java4.tothefuture.from7to8;
+
+public class Person {
+    private Integer age;
+    private String name;
+
+    public Person(Integer age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
