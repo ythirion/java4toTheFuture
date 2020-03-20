6a422d1753d9f1ec3bac771b38dc4880d9971c82
Adrien Muller <amuller@macbook-pro-de-adrien.home>
2020-03-20 11:05:01 +0100

    suppression soluces du master

---
+++ src/main/java/java4/tothefuture/from4to5/User.java
@@ -0,0 +1,31 @@
+package java4.tothefuture.from4to5;
+
+public class User {
+    private Integer id;
+    private String email;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/java4/tothefuture/from4to5/UserNew.java
+++
@@ -1,7 +0,0 @@
-package java4.tothefuture.from4to5;
-
-public class UserNew {
-    private Integer id;
-    private String email;
-    private String name;
-}
