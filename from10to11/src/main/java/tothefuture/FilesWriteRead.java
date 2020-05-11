b00ecb8d37d3e0ec7abd6f4237eafe899c396a7d
Rodislav MOLDOVAN <rodislav@gmail.com>
2020-05-11 09:19:33 +0200

    optimize imports

--- from10to11/src/main/java/tothefuture/FilesWriteRead.java
+++ from10to11/src/main/java/tothefuture/FilesWriteRead.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static java.lang.System.getProperty;
-import static java.nio.charset.Charset.defaultCharset;
 
 public class FilesWriteRead {
 
