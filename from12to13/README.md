e25ab3ab8b86c203d46a3fd005480db951d5aa62
Rodislav MOLDOVAN <rodislav@gmail.com>
2020-05-11 10:47:46 +0200

    added java 13 examples

---
+++ from12to13/README.md
@@ -0,0 +1,9 @@
+### Note
+
+You'll need JDK 12 for this module, easiest way is to get it from
+
+https://jdk.java.net/archive/
+
+This module requires JDK 12.
+
+https://www.journaldev.com/28666/java-12-features
---
+++ from12to13/src/main/java/tothefuture/SwitchExpressions.java
@@ -0,0 +1,22 @@
+package tothefuture;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class SwitchExpressions {
+
+    public static void main(String[] args) {
+        var me = 4;
+        var operation = "squareMe";
+        var result = switch (operation) {
+            case "doubleMe" -> {
+                yield me * 2; // example with yield
+            }
+            case "squareMe" -> me * me; // yield is "inferred", works line a regular lambda expression
+            default -> me;
+        };
+
+        System.out.println(result);
+    }
+
+}
